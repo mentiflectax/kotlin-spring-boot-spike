d0152a84f2220bb4f15b9e8a74c1318f8990df2b
Dmitrii Pisarenko <dp@dpisarenko.com>
2022-04-20 13:18:52 +0200

    Added a simple REST controller.

---
+++ src/main/kotlin/com/dpisarenko/kotlinspringbootspike/TestController.kt
@@ -0,0 +1,16 @@
+package com.dpisarenko.kotlinspringbootspike
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.Date
+
+@RestController
+@RequestMapping("/api/hello")
+class TestController {
+
+    @GetMapping
+    fun hello(): String {
+        return Date().time.toString()
+    }
+}
